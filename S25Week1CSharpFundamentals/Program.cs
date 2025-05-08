d5742a186ee3c05be017b630766d09ef5f7d2295
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-05-08 15:26:14 -0400

    complete

--- S25Week1CSharpFundamentals/Program.cs
+++ S25Week1CSharpFundamentals/Program.cs
@@ -26,6 +26,59 @@
 
             // string interpolation
             Console.WriteLine($"There are {cars} cars, {trucks} trucks and {bikes} bikes");
+
+
+            Console.WriteLine("\n\n");
+
+
+
+            Circle c1 = new Circle();
+            Console.WriteLine(c1.radius);
+
+            c1.radius = 50;
+            Console.WriteLine(c1.radius);
+
+            Circle c2 = new Circle();
+            Console.WriteLine(c2.radius);
+
+            c2.radius = 100;
+            Console.WriteLine(c1.radius);
+
+
+
+            string s1 = "hello";
+            string s2 = s1;
+
+            s2 = "world";
+            Console.WriteLine(s1);  // hello
+            Console.WriteLine(s2);  // world
+
+
+            int j = 5;
+            int k = j;
+
+            k = 10;
+
+
+            string str = null;
+            int num = Convert.ToInt32(str);
+            //int num = int.Parse(str);
+            Console.WriteLine(num);
+
+
+            double amount = 234324.3242;
+            Console.WriteLine(amount.ToString("C"));
+
+            double discount = 0.15;
+            Console.WriteLine(discount.ToString("P0"));
+
+
+            int? z = null;
         }
+    }
+
+    class Circle
+    {
+        public double radius;
     }
 }
