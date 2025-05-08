8546d526d0f7f4e16f2d612541d30d4642e2df1d
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-05-08 14:41:14 -0400

    string interpolarion

--- S25Week1CSharpFundamentals/Program.cs
+++ S25Week1CSharpFundamentals/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int @double = 5;
+
+            int i = 5;
+            double d = 5.5;
+            float f = 5.5F;
+            decimal m = 5.5M;
+            long l = 454332L;
+
+            var n = 5;
+
+
+            Console.WriteLine(@"hello\nworld");
+            Console.WriteLine("\n\n");
+
+
+            int cars = 4, trucks = 6, bikes = 2;
+
+            // string concatenation
+            Console.WriteLine("There are " + cars + " cars, " + trucks + " trucks and " + bikes + " bikes");
+
+            // string interpolation
+            Console.WriteLine($"There are {cars} cars, {trucks} trucks and {bikes} bikes");
         }
     }
 }
